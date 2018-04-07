76925540ecb60f3e364f8ce50c7acb79b2403b84
Nick Yashin <nyashin92@gmail.com>
2018-04-07 17:30:47 +0200

    prepare for publishing

---
+++ webpack.config.build.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: "./src/index.ts",
+  output: {
+      library: "vuexActionNames",
+      libraryTarget: "var",
+      filename: "vuex-action-names.js"
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  module: {
+      rules: [
+          // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+          { test: /\.tsx?$/, loader: "ts-loader" }
+      ]
+  },
+  externals: {
+    vuex: 'vuex',
+  },
+}
